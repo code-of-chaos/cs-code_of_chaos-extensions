1fca49bdad0692aed5684cf7e9c85929935f4fde
Anna Sas <anna.sas.dev@gmail.com>
2024-12-27 16:57:13 +0100

    VersionBump : v0.24.3

--- src/CodeOfChaos.Extensions.AspNetCore/LoggingOverrideExtensions.cs
+++ src/CodeOfChaos.Extensions.AspNetCore/LoggingOverrideExtensions.cs
@@ -38,7 +38,8 @@ public static class LoggingOverrideExtensions {
         builder.Logging.AddSerilog(Log.Logger);
         builder.Services.AddSingleton(Log.Logger);
         builder.Services.AddSingleton<IHostedService, ApplicationShutdownLoggerCleanup>();// Ensure cleanup
-
+        builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(Log.Logger));
+        
         return builder;
     }
     
