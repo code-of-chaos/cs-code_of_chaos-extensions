b1eb5f898cd1b430b6ae4eaf3b8a9afbbef2a0a2
Anna Sas <anna.sas.dev@gmail.com>
2024-12-29 17:16:52 +0100

    VersionBump : v0.27.0

--- src/CodeOfChaos.Extensions.EntityFrameworkCore/LinqExtensions.cs
+++ src/CodeOfChaos.Extensions.EntityFrameworkCore/LinqExtensions.cs
@@ -15,39 +15,44 @@ public static class LinqExtensions {
             ? source.Include(include)
             : source;
 
-    public static IQueryable<T> ConditionalWhere<T>(this IQueryable<T> source, bool condition, Expression<Func<T, bool>> predicate) =>
-        condition
+    public static IQueryable<T> ConditionalWhere<T>(this IQueryable<T> source, bool condition, Expression<Func<T, bool>> predicate) 
+        => condition
             ? source.Where(predicate)
             : source;
 
-    public static IQueryable<T> ConditionalTake<T>(this IQueryable<T> source, bool condition, int count) =>
-        condition
+    public static IQueryable<T> ConditionalTake<T>(this IQueryable<T> source, bool condition, int count) 
+        => condition
             ? source.Take(count)
             : source;
 
-    public static IQueryable<T> ConditionalTake<T>(this IQueryable<T> source, bool condition, Range range) =>
-        condition
+    public static IQueryable<T> ConditionalTake<T>(this IQueryable<T> source, bool condition, Range range) 
+        => condition
             ? source.Take(range)
             : source;
 
-    public static IQueryable<T> ConditionalOrderBy<T>(this IQueryable<T> source, bool condition, Expression<Func<T, object>> orderBy) =>
-        condition
+    public static IQueryable<T> ConditionalOrderBy<T>(this IQueryable<T> source, bool condition, Expression<Func<T, object>> orderBy)
+        => condition
             ? source.OrderBy(orderBy)
             : source;
 
-    public static IQueryable<TSource> ConditionalOrderBy<TSource, TKey>(this IQueryable<TSource> source, bool condition, Expression<Func<TSource, TKey>> orderBy, IComparer<TKey>? comparer) =>
-        condition
+    public static IQueryable<TSource> ConditionalOrderBy<TSource, TKey>(this IQueryable<TSource> source, bool condition, Expression<Func<TSource, TKey>> orderBy, IComparer<TKey>? comparer)
+        => condition
             ? source.OrderBy(orderBy, comparer)
             : source;
 
-    public static IQueryable<TSource> ConditionalOrderByNotNull<TSource, TKey>(this IQueryable<TSource> source, Expression<Func<TSource, TKey>>? orderBy) =>
-        orderBy is not null
+    public static IQueryable<TSource> ConditionalOrderByNotNull<TSource, TKey>(this IQueryable<TSource> source, Expression<Func<TSource, TKey>>? orderBy) 
+        => orderBy is not null
             ? source.OrderBy(orderBy)
             : source;
 
 
-    public static IQueryable<TSource> ConditionalQueryable<TSource>(this IQueryable<TSource> source, bool condition, Func<IQueryable<TSource>, IQueryable<TSource>> queryableFunc) =>
-        condition
+    public static IQueryable<T> ConditionalQueryable<T>(this IQueryable<T> source, bool condition, Func<IQueryable<T>, IQueryable<T>> queryableFunc)
+        => condition
             ? queryableFunc(source)
+            : source;
+
+    public static IQueryable<T> ConditionalReverse<T>(this IQueryable<T> source, bool reverse) 
+        => reverse
+            ? source.Reverse()
             : source;
 }
