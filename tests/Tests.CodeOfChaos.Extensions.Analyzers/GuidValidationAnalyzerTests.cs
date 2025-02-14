c0aab77c01003fdd8c00999cbf82989ce0f02963
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-02-14 01:33:30 +0100

    Feat: GuidValidationAnalyzerTests

---
+++ tests/Tests.CodeOfChaos.Extensions.Analyzers/GuidValidationAnalyzerTests.cs
@@ -0,0 +1,58 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.Extensions.Analyzers;
+using CodeOfChaos.Testing.TUnit;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace Tests.CodeOfChaos.Extensions.Analyzers;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class GuidValidationAnalyzerTests {
+    [Test]
+    public async Task GuidValidationAnalyzer_ShouldNotSetDiagnostic() {
+        // Arrange
+        var runner = new RoslynCompilationRunner()
+            .AddDocument("Test.cs", """
+                namespace TestProject;
+
+                public class TestClass {
+                    public Guid GuidField = "abc37c26-ed0c-47af-8e0a-176d2658324c".ToGuid();
+                }
+                """)
+            .AddDiagnosticAnalyzer<GuidValidationAnalyzer>();
+
+        // Act
+        CompilationWithAnalyzers compilation = await runner.GetCompilationWithAnalyzersAsync();
+        ImmutableArray<Diagnostic> diagnostics = await compilation.GetAllDiagnosticsAsync();
+
+        // Assert
+        await Assert.That(diagnostics).DoesNotContainDiagnostic("CODE001");
+    }
+
+    [Test]
+    public async Task GuidValidationAnalyzer_ShouldSetDiagnostic() {
+        // Arrange
+        var runner = new RoslynCompilationRunner()
+            .AddDocument("Test.cs", """
+                namespace TestProject;
+
+                public class TestClass {
+                    public Guid GuidField = "INVALID GUID".ToGuid();
+                }
+                """)
+            .AddDiagnosticAnalyzer<GuidValidationAnalyzer>();
+
+        // Act
+        CompilationWithAnalyzers compilation = await runner.GetCompilationWithAnalyzersAsync();
+        ImmutableArray<Diagnostic> diagnostics = await compilation.GetAllDiagnosticsAsync();
+
+        // Assert
+        await Assert.That(diagnostics).ContainsDiagnostic("CODE001");
+        
+    }
+}
