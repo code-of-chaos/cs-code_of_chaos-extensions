c6f2917aca962bec17497ff9407fbd4096fc42b4
Anna Sas <anna.sas.dev@gmail.com>
2025-02-20 16:38:25 +0100

    VersionBump : v0.39.4

--- src/CodeOfChaos.Extensions.Serilog/Enrichers/PaddedSectionEnricher.cs
+++ src/CodeOfChaos.Extensions.Serilog/Enrichers/PaddedSectionEnricher.cs
@@ -23,6 +23,8 @@ public class PaddedSectionEnricher : ILogEventEnricher {
     // Methods
     // -----------------------------------------------------------------------------------------------------------------
     public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory) {
+        // Return if the Section has already been set
+        if (logEvent.Properties.TryGetValue("Section", out LogEventPropertyValue? _)) return;
         if (!logEvent.Properties.TryGetValue("SourceContext", out LogEventPropertyValue? sectionProperty)) {
             // If "Section" is not defined, fallback to default value
             sectionProperty = new ScalarValue(string.Empty);
