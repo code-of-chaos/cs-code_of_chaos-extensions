0bb69614c53a3a78df211cb9a841e5bc74a7e467
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-03-14 20:33:18 +0100

    Revert "Change: Test Runner optimization"
    This reverts commit 63f08324a9f17604156bd8cf6d3231c172cfb4ba.

---
+++ tests/Tests.CodeOfChaos.Extensions.DependencyInjection/Attributes/FactoryCreatedServiceAttributeTest.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.Extensions.DependencyInjection;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests.CodeOfChaos.Extensions.DependencyInjection.Attributes;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[TestSubject(typeof(FactoryCreatedServiceAttribute<,>))]
+public class FactoryCreatedServiceAttributeTest {
+    [Test]
+    public async Task FactoryCreatedServiceAttribute_PropertiesAreSetCorrectly() {
+        // Arrange
+        var attribute = new FactoryCreatedServiceAttribute<MyServiceFactory, IMyService>(ServiceLifetime.Scoped);
+
+        // Act
+        ServiceLifetime lifetime = attribute.Lifetime;
+        Type factoryType = attribute.FactoryType;
+        Type serviceType = attribute.ServiceType;
+
+        // Assert
+        await Assert.That(lifetime).IsEqualTo(ServiceLifetime.Scoped);
+        await Assert.That(factoryType).IsEqualTo(typeof(MyServiceFactory));
+        await Assert.That(serviceType).IsEqualTo(typeof(IMyService));
+    }
+
+    [Test]
+    public async Task FactoryCreatedServiceAttribute_CanBeAppliedToClass() {
+        // Act
+        object[] attributes = typeof(SampleServiceWithAttribute).GetCustomAttributes(typeof(FactoryCreatedServiceAttribute<MyServiceFactory, IMyService>), false);
+        var attribute = attributes.FirstOrDefault() as FactoryCreatedServiceAttribute<MyServiceFactory, IMyService>;
+        
+        // Assert
+        await Assert.That(attributes).IsNotEmpty().And.HasSingleItem();
+        await Assert.That(attribute).IsNotNull();
+        await Assert.That(attribute?.Lifetime).IsEqualTo(ServiceLifetime.Singleton);
+    }
+
+    // Dummy class to test attribute application
+    [FactoryCreatedService<MyServiceFactory, IMyService>(ServiceLifetime.Singleton)]
+    private class SampleServiceWithAttribute;
+
+    public interface IMyService;
+
+    public class MyService : IMyService;
+
+    public class MyServiceFactory : IFactoryService<IMyService> {
+        public IMyService Create() => new MyService();
+    }
+}
