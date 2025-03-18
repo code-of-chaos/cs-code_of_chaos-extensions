6cdd2d02ff11abcbe3a06a3b2ed628497f6d8d27
Anna Sas <anna.sas.dev@gmail.com>
2025-03-18 14:16:33 +0100

    Feat: ClaimsPrincipalExtensions

---
+++ src/CodeOfChaos.Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using System.Security.Claims;
+
+namespace CodeOfChaos.Extensions;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class ClaimsPrincipalExtensions {
+    public static IEnumerable<string> FindAllValues(this ClaimsPrincipal principal, string type) {
+        IEnumerable<Claim> claims = principal.FindAll(type);
+        return claims.Select(c => c.Value);
+    }
+}
