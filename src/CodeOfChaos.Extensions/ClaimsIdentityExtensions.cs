ae98914fbb3394ddcedb81d96b9f3353afb59b76
Anna Sas <anna.sas.dev@gmail.com>
2025-03-18 14:14:03 +0100

    Feat: ClaimsIdentityExtensions

--- src/CodeOfChaos.Extensions/ClaimsIdentityExtensions.cs
+++ src/CodeOfChaos.Extensions/ClaimsIdentityExtensions.cs
@@ -9,6 +9,27 @@ namespace CodeOfChaos.Extensions;
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
 public static class ClaimsIdentityExtensions {
+
+    public static ClaimsIdentity AddClaim(this ClaimsIdentity identity, Claim claim) {
+        identity.AddClaim(claim);
+        return identity;
+    }
+
+    public static ClaimsIdentity AddClaims(this ClaimsIdentity identity, IEnumerable<Claim> claims) {
+        identity.AddClaims(claims);
+        return identity;
+    }
+    
+    public static ClaimsIdentity RemoveClaim(this ClaimsIdentity identity, Claim? claim) {
+        identity.RemoveClaim(claim);
+        return identity;
+    }
+
+    public static IEnumerable<string> FindAllValues(this ClaimsIdentity identity, string type) {
+        IEnumerable<Claim> claims = identity.FindAll(type);
+        return claims.Select(c => c.Value);
+    }
+    
     public static ClaimsIdentity AddClaim(this ClaimsIdentity identity, string type, string value) {
         identity.AddClaim(new Claim(type, value));
         return identity;
