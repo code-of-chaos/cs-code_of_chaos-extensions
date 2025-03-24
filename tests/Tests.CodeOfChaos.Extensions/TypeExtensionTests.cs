a31a34b77d4f4c7c9869ba138031ff48ee1b0291
Anna Sas <anna.sas.dev@gmail.com>
2025-03-24 15:03:37 +0100

    Feat: MatchesGenericType

---
+++ tests/Tests.CodeOfChaos.Extensions/TypeExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+namespace Tests.CodeOfChaos.Extensions;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class TypeExtensionTests {
+    [Test]
+    public async Task MatchesGenericType_ShouldReturnTrue() {
+        // Arrange
+        var type = typeof(List<>);
+        var genericType = typeof(List<>);
+        
+        // Act
+        var result = type.MatchesGenericType(genericType);
+
+        // Assert
+        await Assert.That(result).IsTrue();
+    }
+}
