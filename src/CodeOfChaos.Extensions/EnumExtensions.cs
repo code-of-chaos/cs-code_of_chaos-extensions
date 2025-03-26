9bc4c55b5284b1cd10406e972e9775eb74ce86e5
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-03-26 22:52:22 +0100

    Revert "Feat: HasFlag"
    This reverts commit 5c93a37e5d25bf29a8c3ba45296e6f73b84c6420.

--- src/CodeOfChaos.Extensions/EnumExtensions.cs
+++ src/CodeOfChaos.Extensions/EnumExtensions.cs
@@ -10,8 +10,7 @@ namespace System;
 // ---------------------------------------------------------------------------------------------------------------------
 public static class EnumExtensions {
     private static readonly ConcurrentDictionary<Type, Array> EnumValuesCache = new();
-    private static readonly ConcurrentDictionary<Type, bool> IsFlagsEnumCache = new();
-    
+
     /// <summary>
     ///     Retrieves all values of the specified enum type from the cache, falling back to reflection if uncached.
     /// </summary>
@@ -50,26 +49,4 @@ public static class EnumExtensions {
     /// </summary>
     public static List<T> GetFlagsAsList<T>(this T flagEnum, bool excludeZeroValue = true) where T : struct, Enum
         => GetFlags(flagEnum, excludeZeroValue).ToList();
-
-    /// <summary>
-    /// Determines whether the specified flags are set in the given enum value.
-    /// </summary>
-    /// <typeparam name="T">The type of the Enum.</typeparam>
-    /// <param name="flagEnum">The enum value to be inspected.</param>
-    /// <param name="flag">The flag or flags to check for in the enum value.</param>
-    /// <returns>A boolean indicating whether the specified flags are set in the enum value.</returns>
-    /// <exception cref="ArgumentException">
-    /// Thrown if the provided enum type does not have the [Flags] attribute.
-    /// </exception>
-    public static bool HasFlag<T>(this T flagEnum, T flag) where T : struct, Enum {
-        bool isFlagsEnum = IsFlagsEnumCache.GetOrAdd(typeof(T), t => t.IsDefined(typeof(FlagsAttribute), false));
-        if (!isFlagsEnum) throw new ArgumentException("The provided enum type must have the [Flags] attribute.", nameof(flagEnum));
-
-        // Convert enums to unsigned numeric types to perform bitwise comparison
-        ulong flagEnumValue = Convert.ToUInt64(flagEnum);
-        ulong flagValue = Convert.ToUInt64(flag);
-
-        // Perform the bitwise comparison
-        return (flagEnumValue & flagValue) == flagValue;
-    }
 }
