2b297a0aaa3751b38fd33f0619f054a9248e9ff2
Anna Sas <anna.sas.dev@gmail.com>
2025-03-26 17:18:02 +0100

    Feat: GroupExtensions for Regex
    **Twitch Redeems**
    - brought to you by `fluffycat_1337`
    - brought to you by `jeroenvanwissen` with : "I forgot what I wanted to put here"

---
+++ src/CodeOfChaos.Extensions/RegularExpressions/GroupExtensions.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using System.Diagnostics.CodeAnalysis;
+
+// ReSharper disable once CheckNamespace
+namespace System.Text.RegularExpressions;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class GroupExtensions {
+    public static bool TryGetValue(this Group group,[NotNullWhen(true)] out string? value) {
+        if (group.Success) {
+            value = group.Value;
+            return true;
+        }
+        value = null;
+        return false;
+    }
+    
+    public static bool TryGetValueSpan(this Group group, out ReadOnlySpan<char> value) {
+        if (group.Success) {
+            value = group.ValueSpan;
+            return true;
+        }
+        value = default;
+        return false;
+    }
+}
