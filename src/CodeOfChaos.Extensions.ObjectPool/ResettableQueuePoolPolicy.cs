7b222281d9c8126a11293bde0803dc6ad30f3f46
Anna Sas <anna.sas.dev@gmail.com>
2025-04-23 14:02:02 +0200

    Feat: Add custom object pool policies for Stack and Queue types

---
+++ src/CodeOfChaos.Extensions.ObjectPool/ResettableQueuePoolPolicy.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Microsoft.Extensions.ObjectPool;
+
+namespace CodeOfChaos.Extensions.ObjectPool;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class ResettableQueuePoolPolicy<T, TItem> : IPooledObjectPolicy<T> 
+    where T : Queue<TItem>, new()
+    where TItem : IResettable 
+{
+    public T Create() => new();
+    public bool Return(T obj) {
+        while (obj.TryDequeue(out TItem? item)) {
+            item.TryReset();
+        }
+        obj.Clear();
+        return obj.Count == 0;
+    }
+}
---
+++ src/CodeOfChaos.Extensions.ObjectPool/ResettableStackPoolPolicy.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Microsoft.Extensions.ObjectPool;
+
+namespace CodeOfChaos.Extensions.ObjectPool;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class ResettableStackPoolPolicy<T, TItem> : IPooledObjectPolicy<T> 
+    where T : Stack<TItem>, new()
+    where TItem : IResettable 
+{
+    public T Create() => new();
+    public bool Return(T obj) {
+        while (obj.TryPop(out TItem? item)) {
+            item.TryReset();
+        }
+        obj.Clear();
+        return obj.Count == 0;
+    }
+}
