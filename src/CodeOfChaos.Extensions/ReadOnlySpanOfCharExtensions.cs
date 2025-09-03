71dc3c5fdff4125be7e44d0f2c74aafdff06e68e
Anna Sas <anna.sas.dev@gmail.com>
2025-09-03 14:41:18 +0200

    Feat: Add `ReadOnlySpan<char>` extension methods for escaped character checks with tests

---
+++ src/CodeOfChaos.Extensions/ReadOnlySpanOfCharExtensions.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+// ReSharper disable once CheckNamespace
+namespace System;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class ReadOnlySpanOfCharExtensions {
+    public static bool IsNotEscapedCharacterAtIndex(this scoped ReadOnlySpan<char> source, int atIndex) {
+        int backslashCount = 0;
+        for (int i = atIndex - 1; i >= 0 && source[i] is '\\'; i--) {
+            backslashCount++;
+        }
+        return backslashCount % 2 == 0;
+    }
+
+    public static bool IsEscapedCharacterAtIndex(this scoped ReadOnlySpan<char> source, int atIndex) {
+        return !IsNotEscapedCharacterAtIndex(source, atIndex);
+    }
+    
+}
